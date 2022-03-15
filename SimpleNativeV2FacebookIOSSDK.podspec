310cc1374f23faf6747947ec4a3659bd5953efa6
xxx <xxx@x.com>
2022-03-15 20:37:51 +0800

    read to push

---
+++ SimpleNativeV2FacebookIOSSDK.podspec
@@ -0,0 +1,18 @@
+Pod::Spec.new do |spec|
+  spec.name = "SimpleNativeV2FacebookIOSSDK"
+  spec.version="0.3.0"
+  spec.summary= "Simple SDK V2 for include Facebook"
+  spec.description= <<-DESC
+  Simple SDK V2 for include Facebook, please includ the core sdk more
+  DESC
+  spec.homepage= 'https://github.com/bepic-games/SimpleNativeV2FacebookIOSSDK'
+  spec.license= { :type => 'MIT', :file => 'LICENSE' }
+  spec.author = { "yilang" => "yilang@bepic.cc" }
+  spec.ios.deployment_target = "11.0"
+  spec.source = { :git => "https://github.com/bepic-games/SimpleNativeV2FacebookIOSSDK.git", :tag => "#{spec.version}" }
+  spec.source_files= 'SimpleNativeV2FacebookIOSSDK/*.{framework}/Headers/*.h'
+  spec.vendored_frameworks = 'SimpleNativeV2FacebookIOSSDK/*.{framework}'
+  spec.public_header_files = 'SimpleNativeV2FacebookIOSSDK/*.{framework}/Headers/*.h'
+  spec.pod_target_xcconfig = { 'VALID_ARCHS' => 'arm64' }
+  spec.dependency 'FBSDKLoginKit','13.0.0'
+end
---
+++ SimpleNativeV2FacebookIOSSDK/SimpleNativeFacebook.framework/Headers/SimpleNativeFacebook.h
@@ -0,0 +1,34 @@
+//
+//  SimpleNativeFacebook.h
+//  SimpleNativeFacebook
+//
+//  Created by qus on 2022/3/10.
+//
+
+#import <Foundation/Foundation.h>
+#import <SimpleNativeBase/SimpleNativeBase.h>
+#import <SimpleNativeUserPayment/LoginServices.h>
+
+@interface FacebookLogin<ILogin>: NSObject
+@property ClientLoginParamsSuccess success;
+@property CallbackFail fail;
+
++(instancetype)sharedInstance;
+
+@end
+
+@interface FacebookAppDelegate<SimpleSDKAppDelegateProtocol>: NSObject
+@end
+
+@interface FacebookThirdUploadLogger<ThirdUploadLoggerProtocol>:NSObject
+@end
+
+//! Project version number for SimpleNativeFacebook.
+FOUNDATION_EXPORT double SimpleNativeFacebookVersionNumber;
+
+//! Project version string for SimpleNativeFacebook.
+FOUNDATION_EXPORT const unsigned char SimpleNativeFacebookVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SimpleNativeFacebook/PublicHeader.h>
+
+
